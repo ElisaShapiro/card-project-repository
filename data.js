f0a737b399714084312f0cf8d0f330fa7bb2394c
Elisa Shapiro <elisashapiro@gmail.com>
2021-07-04 13:06:19 -0400

    added card info to data.js file

---
+++ data.js
@@ -0,0 +1,61 @@
+const magicCardDealer = {
+    contact: "MagicCard@magiccard.com",
+    hours: "24/7 ",
+    inventory: [
+        {
+            id: "164",
+            name: "Advocate of the Beast",
+            color: "green",
+            manaCost: 2, 
+            flavorText: "I am neither their drover nor their leader. To these majestic beats, I am simply their herdmate and nothing more",
+            image: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=370738&type=card",
+            effect: "At the beginning of your end step, put a +1/+1 counter on target Beast create you control",
+            power: 2,
+            toughness: 3
+        },
+        {
+            id: "324",
+            name: "Alela, Artful Provocateur",
+            color: "yellow",
+            manaCost: 4,
+            flavorText: "Rankle/'s pranks are child/'s play. My games will topple kingdoms.",
+            image: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=476042&type=card",
+            effect: "Flying, deathtouch, lifelink. Other creatures you control with flying get+1/+0. Whenever you cast an artifact or enchantment spell, create a 1/1/ blue Faerie create token with flying",
+            power: 2,
+            toughness: 3
+        },
+        {
+            id: "203",
+            name: "Black Cat",
+            color: "black",
+            manaCost: 1, 
+            flavorText: "Its last life is spent tormenting your dreams.",
+            image: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=489209&type=card",
+            effect: "When Black Cat dies, target opponent discards a card at random.",
+            power: 1,
+            toughness: 1
+        },
+        {
+            id: "129",
+            name: "Chandra/'s Embercat",
+            color: "orange",
+            manaCost: 1, 
+            flavorText: "Furballs? Try drealing with fireballs. - Chandra Nalaar",
+            image: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=466883&type=card",
+            effect: "Add fire mana. Spend this mana only to cast an Elemental spell or a Chandra planeswalker spell.",
+            power: 2,
+            toughness: 2
+        },
+        {
+            id: "151",
+            name: "Elite Cat Warrior",
+            color: "green",
+            manaCost: 2, 
+            flavorText: "Hear that? No? That/'s a cat warrior",
+            image: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=221917&type=card",
+            effect: "Forestwalk (If defending player has any forests in play, Elite Cat Warrior cannot be intercepted. Forests are in play regardless of whether they are tapped or untapped.)",
+            power: 2,
+            toughness: 3
+        },
+    ]
+}
